23d763ae248743bf56294de51898be7b12399f74
Robert Norris <rnorris7756@gmail.com>
2017-01-26 02:01:05 -0800

    Fixed issue where the server wouldn't save the map on ctrl-c or docker-compose stop.

--- init.sh
+++ init.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+#We use exec here to replace the init script with the factorio executable.
+#This allows factorio to receive SIGINT/SIGTERM and shut down gracefully when
+#the container receives a signal to shut down.
 
 if [ ! -f $SAVEFILE ]; then
-  /opt/factorio/bin/x64/factorio --create $SAVEFILE
+  exec /opt/factorio/bin/x64/factorio --create $SAVEFILE
+else
+  exec /opt/factorio/bin/x64/factorio --start-server $SAVEFILE
 fi
-/opt/factorio/bin/x64/factorio --start-server $SAVEFILE
